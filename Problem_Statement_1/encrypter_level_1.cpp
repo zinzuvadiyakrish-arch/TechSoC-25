6a4c99dd8be332c8617e4b393ca77f607d8e4e7e
zinzuvadiyakrish-arch <zinzuvadiyakrish@gmail.com>
2025-09-01 09:41:58 +0530

    Update encrypter_level_1.cpp

--- Problem_Statement_1/encrypter_level_1.cpp
+++ Problem_Statement_1/encrypter_level_1.cpp
@@ -2,33 +2,34 @@
 int main() {
     char letter;
     int i = 0;
+    int j ;
     std::string statement;
     std::cout<<"Please enter a line of text : ";
     std::getline(std::cin, statement);
+    std::cout<<"Please enter the shift in alphabets : ";
+    std::cin>>j;
+    while (j>26) {
+        j=j-26;
+    }
     while (i<statement.length()) {
+
         letter = statement.at(i);
-         i=i+1;
-        if ((int(letter) > 64 && int(letter) < 91) ||
-            (int(letter) > 96 && int(letter) < 123)) {
-            if (int(letter) == 121 ) {
-                std::cout<<"a";
+        if(int(letter)+j>122){
+            letter=letter+j-26;
+             std::cout<<char(letter);
             }
-            else if (int(letter) == 122 ) {
-                std::cout<<"b";
-            }
-            else if (int(letter) == 89) {
-                std::cout<<"A";
-            }
-            else if (int(letter) == 90 ) {
-                std::cout<<"B";
-            }
-            else {
-                letter = letter+2;
-                std::cout<<char(letter);
-            }
+        else if (int(letter)+j>90 && int(letter)<91){
+            letter=letter+j-26;
+            std::cout<<char(letter);
+        }
+
+        else if (int (letter)==32) {
+            std::cout<<char(letter);
         }
         else {
-                std::cout<<letter;
+                letter = letter+j;
+                std::cout<<char(letter);
             }
-    }
+        i=i+1;
+        }
 }
